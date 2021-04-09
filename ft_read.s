14472e2c389c0779b1751825f563ee4415696b89
Loïc <lbertran@student.42lyon.fr>
2021-04-09 10:38:07 +0200

    ;

--- ft_read.s
+++ ft_read.s
@@ -3,6 +3,6 @@ section .text
 global _ft_read
 
 _ft_read:
-	mov rax, 0x02000003
+	mov rax, 0x02000003 ; mov sys_read to rax
 	syscall
 	ret
--- ft_write.s
+++ ft_write.s
@@ -3,6 +3,6 @@ section .text
 global _ft_write
 
 _ft_write:
-	mov rax, 0x02000004
+	mov rax, 0x02000004	; mov sys_write to rax
 	syscall
 	ret
